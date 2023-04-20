53036a63f10f08d2385fd1bc3f1c81d29e792389
Juan Moreno <earth001@gmail.com>
2023-04-20 01:04:16 -0300

    Updated format

--- src/test/java/ve/com/proitcsolution/Ch1BuildingBlocksTest.java
+++ src/test/java/ve/com/proitcsolution/Ch1BuildingBlocksTest.java
@@ -8,39 +8,42 @@ import org.junit.jupiter.api.Test;
 
 class Ch1BuildingBlocksTest {
 
-    private static final Predicate<String> REGEX_LEADING_WHITESPACE = Pattern.compile("\\s+.*")
-        .asMatchPredicate();
-    private static final Predicate<String> REGEX_TRAILING_WHITESPACE = Pattern.compile(".*\\s+")
-        .asMatchPredicate();
-    private static final String LINEBREAK_MATCHER = "\\R";
+  private static final Predicate<String> REGEX_LEADING_WHITESPACE =
+      Pattern.compile("\\s+.*").asMatchPredicate();
+  private static final Predicate<String> REGEX_TRAILING_WHITESPACE =
+      Pattern.compile(".*\\s+").asMatchPredicate();
+  private static final String LINEBREAK_MATCHER = "\\R";
 
-    @Test
-    void verifyTextBlock() {
-        var textBlock = """
+  @Test
+  void verifyTextBlock() {
+    var textBlock = """
             squirrel \s
             pigeon \
             termite""";
-        var textBlock2 = """
+    var textBlock2 =
+        """
             squirrel
               pigeon
             termite
             """;
 
-        assertThat(textBlock).hasLineCount(2);
-        assertThat(textBlock.split(LINEBREAK_MATCHER)).anyMatch(REGEX_TRAILING_WHITESPACE)
-            .noneMatch(REGEX_LEADING_WHITESPACE);
+    assertThat(textBlock).hasLineCount(2);
+    assertThat(textBlock.split(LINEBREAK_MATCHER))
+        .anyMatch(REGEX_TRAILING_WHITESPACE)
+        .noneMatch(REGEX_LEADING_WHITESPACE);
 
-        assertThat(textBlock2).hasLineCount(3);
-        assertThat(textBlock2.split(LINEBREAK_MATCHER)).anyMatch(REGEX_LEADING_WHITESPACE)
-            .noneMatch(REGEX_TRAILING_WHITESPACE);
-    }
+    assertThat(textBlock2).hasLineCount(3);
+    assertThat(textBlock2.split(LINEBREAK_MATCHER))
+        .anyMatch(REGEX_LEADING_WHITESPACE)
+        .noneMatch(REGEX_TRAILING_WHITESPACE);
+  }
 
-    @Test
-    void verifyPrimitive() {
-        int number1 = Integer.parseInt("15"); // Primitive, all .parse* methods return primitives.
-        var number2 = Integer.valueOf("15");
+  @Test
+  void verifyPrimitive() {
+    int number1 = Integer.parseInt("15"); // Primitive, all .parse* methods return primitives.
+    var number2 = Integer.valueOf("15");
 
-        assertThat(number1).isEqualTo(15);
-        assertThat(number2.getClass().isPrimitive()).isFalse();
-    }
+    assertThat(number1).isEqualTo(15);
+    assertThat(number2.getClass().isPrimitive()).isFalse();
+  }
 }
